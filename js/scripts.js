59a75cc7da44a68f914d4d5412d2decc27d9b7b9
Adam Baker <adam.baker97202@gmail.com>
2016-10-20 09:12:12 -0700

    Made with prompts, JS, if statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,15 @@
-var flavor = $("input:radio[name=animal]:checked").val();
+$(document).ready(function() {
+ var input = prompt("What is your favorite animal? Turtles, Snakes, or Insects?");
+   var animalType = input.toUpperCase();
+
+   if (animalType === "TURTLES") {
+    $(".turtles").show();
+   } else if (animalType === "SNAKES") {
+    $(".snakes").show();
+   } else if (animalType === "INSECTS") {
+    $(".insects").show();
+  }
+    else {
+     alert("Please select one of the three options, Turtles, Snakes, or Insects")
+    }
+ });
